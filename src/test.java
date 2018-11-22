0faf9cab75a73f1b2bfb1d374992754d8a6ba63f
ViktorLandstrom <viklan1129@gmail.com>
2018-11-22 11:16:27 +0100

    commit

--- src/test.java
+++ src/test.java
@@ -3,5 +3,6 @@ import javax.swing.*;
 public class test{
     public static void main(String[] args){
         JOptionPane.showMessageDialog(null,"Hello World");
+        JOptionPane.showMessageDialog(null, "socke","YESINEED",JOptionPane.WARNING_MESSAGE);
     }
 }
